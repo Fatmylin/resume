98774442df590a0716ad5241e4d371d91210c57b
Fatmylin <jimmy0921956909@gmail.com>
2017-03-29 17:14:41 +0800

    四更 03/29

--- resume.js
+++ resume.js
@@ -3,11 +3,13 @@ $('.second').mouseenter(function(){
  
   $(".face").addClass("animate");
   $(".intro").addClass("animate");
+  $(".sintro").addClass("animate2");
 });
 $(".second").mouseleave(function(){
  
   $(".face").removeClass("animate");
   $(".intro").removeClass("animate");
+  $(".sintro").removeClass("animate2");
 });
 
 $(".third").mouseenter(function(){
@@ -17,7 +19,9 @@ $(".third").mouseenter(function(){
   var y = position.top; 
   y-=10;
   // alert(y);
-  $('.selintro').offset({top:y,left:x});     $(".selintro").addClass("animate");
+  $('.selintro').offset({top:y,left:x});     
+  $(".selintro").addClass("animate");
+  $(".sintro2").addClass("animate2");
   
 });
 $(".third").mouseleave(function(){
@@ -28,5 +32,17 @@ $(".third").mouseleave(function(){
   y+=10;
   $('.selintro').offset({top:y,left:x}); 
   $(".selintro").removeClass("animate");
+  $(".sintro2").removeClass("animate2");
   
+});
+
+$('.fourth').mouseenter(function(){
+ 
+  $(".film").addClass("animate");
+  $(".sintro3").addClass("animate2");
+});
+$(".fourth").mouseleave(function(){
+ 
+  $(".film").removeClass("animate");
+  $(".sintro3").removeClass("animate2");
 });
