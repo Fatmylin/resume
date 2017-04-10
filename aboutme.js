31b065075d5cfd32bb4c54d59e35cb31031bd68d
Fatmylin <jimmy0921956909@gmail.com>
2017-04-10 10:33:24 +0800

    0410
    0410

---
+++ aboutme.js
@@ -0,0 +1,52 @@
+
+$("#intro").mouseenter(function(){
+ 
+  $(".profilepic").addClass("animate");
+  $(".experience").addClass("animate");
+  $(".sintro").addClass("animate2");
+});
+$("#intro").mouseleave(function(){
+ 
+  $(".profilepic").removeClass("animate");
+  $(".experience").removeClass("animate");
+  $(".sintro").removeClass("animate2");
+});
+
+$("#skill").mouseenter(function(){
+ 
+  var position = $(".skill").offset();  
+  var x = position.left;  
+  var y = position.top; 
+  y-=10;
+  // alert(y);
+  $(".skill").offset({top:y,left:x});     
+  $(".skill").addClass("animate");
+  $(".b").addClass("animate");
+  $(".sintro2").addClass("animate2");
+  
+});
+$("#skill").mouseleave(function(){
+ 
+  var position = $(".skill").offset();  
+  var x = position.left;  
+  var y = position.top; 
+  y+=10;
+  $(".skill").offset({top:y,left:x}); 
+  $(".skill").removeClass("animate");
+  $(".b").removeClass("animate");
+  $(".sintro2").removeClass("animate2");
+  
+});
+
+$("#works").mouseenter(function(){
+ 
+  $(".b2").addClass("animate");
+  $(".films").addClass("animate");
+  $(".sintro3").addClass("animate2");
+});
+$("#works").mouseleave(function(){
+ 
+  $(".b2").removeClass("animate");
+  $(".films").removeClass("animate");
+  $(".sintro3").removeClass("animate2");
+});
