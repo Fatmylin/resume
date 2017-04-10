44b232c428432bd67b42ac3fda196ef75450a9e7
Fatmylin <jimmy0921956909@gmail.com>
2017-04-11 00:04:22 +0800

    0411 12:04

--- aboutme.js
+++ aboutme.js
@@ -1,4 +1,4 @@
-
+/*
 $("#intro").mouseenter(function(){
  
   $(".profilepic").addClass("animate");
@@ -49,4 +49,37 @@ $("#works").mouseleave(function(){
   $(".b2").removeClass("animate");
   $(".films").removeClass("animate");
   $(".sintro3").removeClass("animate2");
+});*/
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 650) {
+        // apply effects and animations
+		$(".profilepic").addClass("animate");
+		$(".experience").addClass("animate");
+		$(".sintro").addClass("animate2");
+    }
+	
+	if ($(this).scrollTop() > 1200){		  
+		  $(".skill").addClass("animate");
+		  $(".b").addClass("animate");
+		  $(".sintro2").addClass("animate2");
+	}
+	
+	if ($(this).scrollTop() > 2000){		  
+		   $(".b2").addClass("animate");
+		   $(".films").addClass("animate");
+		   $(".sintro3").addClass("animate2");
+	}
+});
+$(document).ready(function() {
+$(".various").fancybox({
+    maxWidth    : 800,
+    maxHeight   : 600,
+    fitToView   : false,
+    width       : '70%',
+    height      : '70%',
+    autoSize    : false,
+    closeClick  : false,
+    openEffect  : 'elastic',
+    closeEffect : 'none'
+});
 });
