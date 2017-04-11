99e6efb7d15f7fe19bd33f03f343913b901fff05
Fatmylin <jimmy0921956909@gmail.com>
2017-04-11 21:54:46 +0800

    0411

--- aboutme.js
+++ aboutme.js
@@ -50,7 +50,22 @@ $("#works").mouseleave(function(){
   $(".films").removeClass("animate");
   $(".sintro3").removeClass("animate2");
 });*/
+ $(window).resize(function(){	 
+ 
+ if ($(window).width() >1100)
+	 {
+		 $(".profilepic").addClass("animate3");
+		 $(".experience").addClass("animate4");
+
+	 }
+	 else if($(window).width() <1100)
+     {
+		 $(".profilepic").removeClass("animate3");
+		 $(".experience").removeClass("animate4");
+
+	 }
 $(window).scroll(function() {
+
     if ($(this).scrollTop() > 650) {
         // apply effects and animations
 		$(".profilepic").addClass("animate");
@@ -70,6 +85,25 @@ $(window).scroll(function() {
 		   $(".sintro3").addClass("animate2");
 	}
 });
+
+$(function(){
+　$(window).load(function(){
+　　$(window).bind('scroll resize', function(){
+　　var $this = $(this);
+　　var $this_Top=$this.scrollTop();
+
+　　//當高度小於100時，關閉區塊 
+   
+　　if($this_Top < 100){
+　　　$('.nav').stop().animate({top:"-65px"});
+　　　}
+　　　　if($this_Top > 100){
+　　　　$('.nav').stop().animate({top:"0px"});
+　　　 }
+　　}).scroll();
+　});
+});
+});/*
 $(document).ready(function() {
 $(".various").fancybox({
     maxWidth    : 800,
@@ -82,4 +116,4 @@ $(".various").fancybox({
     openEffect  : 'elastic',
     closeEffect : 'none'
 });
-});
+});*/
