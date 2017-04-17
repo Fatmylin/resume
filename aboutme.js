f3763d0cba1dc1ddfe65d47cac37d233825a7db1
Fatmylin <jimmy0921956909@gmail.com>
2017-04-17 16:16:14 +0800

    0417 side bar plus

--- aboutme.js
+++ aboutme.js
@@ -1,61 +1,22 @@
-/*
-$("#intro").mouseenter(function(){
- 
-  $(".profilepic").addClass("animate");
-  $(".experience").addClass("animate");
-  $(".sintro").addClass("animate2");
-});
-$("#intro").mouseleave(function(){
- 
-  $(".profilepic").removeClass("animate");
-  $(".experience").removeClass("animate");
-  $(".sintro").removeClass("animate2");
-});
 
-$("#skill").mouseenter(function(){
- 
-  var position = $(".skill").offset();  
-  var x = position.left;  
-  var y = position.top; 
-  y-=10;
-  // alert(y);
-  $(".skill").offset({top:y,left:x});     
-  $(".skill").addClass("animate");
-  $(".b").addClass("animate");
-  $(".sintro2").addClass("animate2");
-  
-});
-$("#skill").mouseleave(function(){
- 
-  var position = $(".skill").offset();  
-  var x = position.left;  
-  var y = position.top; 
-  y+=10;
-  $(".skill").offset({top:y,left:x}); 
-  $(".skill").removeClass("animate");
-  $(".b").removeClass("animate");
-  $(".sintro2").removeClass("animate2");
-  
-});
-
-$("#works").mouseenter(function(){
- 
-  $(".b2").addClass("animate");
-  $(".films").addClass("animate");
-  $(".sintro3").addClass("animate2");
-});
-$("#works").mouseleave(function(){
- 
-  $(".b2").removeClass("animate");
-  $(".films").removeClass("animate");
-  $(".sintro3").removeClass("animate2");
-});*/
  $(window).resize(function(){	 
  
+  if ($(window).width() <860)
+  {
+	  $(".index").addClass("animate");
+	  $(".bar").addClass("animate2");
+  }
+  else
+  {
+	  $(".index").removeClass("animate");
+	  $(".bar").removeClass("animate2");
+  }
  if ($(window).width() >1100)
 	 {
+		
 		 $(".profilepic").removeClass("animate3");
 		 $(".experience").removeClass("animate4");
+		 $("#banner").css("height","800px");
 
 	 }
 	 else if($(window).width() <1100)
@@ -63,6 +24,7 @@ $("#works").mouseleave(function(){
 		
 		 $(".profilepic").addClass("animate3");
 		 $(".experience").addClass("animate4");
+		 $("#banner").css("height","1000px");
 	 }
 	 });
 $(window).scroll(function() {
@@ -104,17 +66,10 @@ $(function(){
 　　}).scroll();
 　});
 });
-/*
-$(document).ready(function() {
-$(".various").fancybox({
-    maxWidth    : 800,
-    maxHeight   : 600,
-    fitToView   : false,
-    width       : '70%',
-    height      : '70%',
-    autoSize    : false,
-    closeClick  : false,
-    openEffect  : 'elastic',
-    closeEffect : 'none'
+
+$(".index").click(function(){
+  $('.navside').stop().animate({right:"0px"});　　　
 });
-});*/
+$(".barside").click(function(){
+  $('.navside').stop().animate({right:"-200px"});　
+});
